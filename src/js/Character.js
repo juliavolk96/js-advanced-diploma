860494b94ec929f9c0ecaf96b1a943801c9462cf
Julia <juliavolk96@gmail.com>
2023-08-21 18:56:41 +0400

    feat: add characters

--- src/js/Character.js
+++ src/js/Character.js
@@ -14,6 +14,9 @@
  */
 export default class Character {
   constructor(level, type = 'generic') {
+    if (new.target === Character) {
+      throw new Error('Character class should not be instantiated directly.')
+    }
     this.level = level;
     this.attack = 0;
     this.defence = 0;
---
+++ src/js/characters/Bowman.js
@@ -0,0 +1,10 @@
+import { construct } from "core-js/fn/reflect";
+import Character from "../Character";
+
+export default class Bowman extends Character {
+  constructor(level) {
+    super(level, 'bowman');
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
